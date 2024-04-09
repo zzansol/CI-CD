eac205ceebe2eb7776c03e9ff63f22b670a21a57
zzansol <chsk9912@gmail.com>
2024-04-09 16:09:37 +0900

    healthCheck

---
+++ src/main/java/com/example/CICD/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.example.CICD.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "health!";
+    }
+}
