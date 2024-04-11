31280e5cbe3bc3bee5b0ee35425315e2d2594835
zzansol <chsk9912@gmail.com>
2024-04-11 17:27:46 +0900

    tag test

--- src/main/java/com/example/CICD/controller/RootController.java
+++ src/main/java/com/example/CICD/controller/RootController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
     @GetMapping("/health")
     public String healthCheck(){
-        return "health!!!!!";
+        return "health!";
     }
 }
